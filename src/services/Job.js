e8d5218b622c9b10436a2518ea4466081e756808
Igashi Michael <mikkybeardless@gmail.com>
2024-07-30 11:47:23 +0100

    add checkBirthday argument

--- src/services/Job.js
+++ src/services/Job.js
@@ -1,12 +1,15 @@
 import Cron from "croner";
 import checkBirthday from "./checkBirthday.js";
-const everyDayJob = () => {
+import { getAllUsers } from "./getUsers.js";
+
+const everyDayJob = async () => {
   const date = new Date();
   const time = date.toLocaleTimeString();
+  const users = await getAllUsers();
   const job = Cron("0 07 * * 0-6", () => {
     // Runs every day at 7AM
     console.log("Happy Birthday, time:", time);
-    checkBirthday();
+    checkBirthday(users);
   });
 };
 export default everyDayJob;
